a5e1b1bc233032eecb9bef2b3e0696e61c682cdd
Deepak Gopalan <gopalan.d@northeastern.edu>
2021-04-18 18:17:45 -0400

    base project

---
+++ src/test/java/com/csye7250/project/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csye7250.project.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
