984fb793e675d601cef77587a9272535b36d85bf
Apoorva Mishra <mishra.ap@northeastern.edu>
2021-04-20 14:45:49 -0400

    changing restcontroller

--- src/main/java/com/csye7250/project/webapp/controller/DomainController.java
+++ src/main/java/com/csye7250/project/webapp/controller/DomainController.java
@@ -3,14 +3,12 @@ package com.csye7250.project.webapp.controller;
 import com.csye7250.project.webapp.entity.Domain;
 import com.csye7250.project.webapp.service.DomainService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class DomainController {
     @Autowired
     private DomainService domainService;
