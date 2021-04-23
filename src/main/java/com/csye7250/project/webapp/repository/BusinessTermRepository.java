263e22a66bc1aa9b3f3c62834170d5142a0bc606
Deepak Gopalan <gopalan.d@northeastern.edu>
2021-04-23 15:36:19 -0400

    changed methods in business terms

--- src/main/java/com/csye7250/project/webapp/repository/BusinessTermRepository.java
+++ src/main/java/com/csye7250/project/webapp/repository/BusinessTermRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface BusinessTermRepository extends CrudRepository<BusinessTerm, Integer> {
 
 
-    BusinessTerm findByPropertyId(int propertyId);
+
 }
